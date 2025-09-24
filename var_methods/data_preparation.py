8be7c397db95bb23465ddce2ad4c597a2641fa55
dawidtr28 <dawidtryka1@gmail.com>
2025-09-24 17:46:42 +0200

    Update data_preparation.py

--- var_methods/data_preparation.py
+++ var_methods/data_preparation.py
@@ -2,9 +2,10 @@ import numpy as np
 import yfinance as yf
 
 def get_data(tickers, weights, period='2y'):
-    prices = yf.download(tickers, period=period,)['Adj Close']
+    prices = yf.download(tickers, period=period,)['Close']
     prices = prices.interpolate(method='linear')
     log_returns = np.log(prices / prices.shift(1)).dropna()
     portfolio_log_returns = log_returns.dot(weights)
 
     return prices, portfolio_log_returns
+
